c97b22cd80243492522d259a074934f3b2666240
Hosana <hosanasas@hotmail.com>
2024-12-10 13:51:56 +0000

    Tela inicial edittext

---
+++ app/src/main/java/com/hosanasas/calculadoraimc/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.hosanasas.calculadoraimc
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
+        val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
+
+        val btnCalcular = findViewById<Button>(R.id.btn_calcular)
+
+        btnCalcular.setOnClickListener {
+            val peso = edtPeso.text
+            val altura = edtAltura.text
+            println("Roque acao do botao" + altura)
+        }
+
+    }
+}
