618890c745e5f75a9771ccd713d6b41e91fd4b6c
Hosana <hosanasas@hotmail.com>
2025-01-13 12:33:12 +0000

    Mostrar classificação

--- app/src/main/java/com/hosanasas/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/hosanasas/calculadoraimc/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.hosanasas.calculadoraimc
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -10,6 +11,7 @@ import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
     private fun show() {
         TODO("Not yet implemented")
     }
@@ -24,32 +26,36 @@ class MainActivity : AppCompatActivity() {
 
         val btnCalcular = findViewById<Button>(R.id.btn_calcular)
 
-        btnCalcular.setOnClickListener {
-            val pesoStr: String = edtPeso.text.toString()
-            val alturaStr: String = edtAltura.text.toString()
+        btnCalcular {
+            setOnClickListener {
+                val pesoStr: String = edtPeso.text.toString(
+                val alturaStr: String = edtAltura.text.toString()
 
-            if(pesoStr == "" || alturaStr == "") {
-                //Mostrar mnensagem para o usuario
-
-                Snackbar
-                    .make(
-                        edtPeso,
-                        text: "Preencha todos os campos",
-                        Snackbar.LENGTH_LONG
-                    )
-                    .show()
+                if (pesoStr == "" || alturaStr == "") {
+                    Snackbar
+                        .make(
+                            edtPeso,
+                            "Preencha todos os campos",
+                            Snackbar.LENGTH_LONG
+                        )
+                        .show()
 
 
-            } else {
-                val peso = pesoStr.toFloat()
-                val altura = alturaStr.toFloat()
+                } else {
+                    val peso = pesoStr.toFloat()
+                    val altura = alturaStr.toFloat()
 
-                val alturaQ2 = altura * altura
-                val resultado = peso / alturaQ2
+                    val alturaQ2 = altura * altura
+                    val resultado = peso / alturaQ2
 
-                println("Roque acao do botao" + resultado)
+                    val intent = Intent(this, ResultActivity::class.java)
+                    intent.putExtra(KEY_RESULT_IMC, resultado)
+                    startActivity(intent)
+
+                    println("Roque acao do botao" + resultado)
+                }
+
             }
-
         }
 
     }
